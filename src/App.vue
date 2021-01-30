0dbd44db8373e29869acbeb6aa59ed57577703bd
pokusaeff.daniil <69844034+iwannabekoshka@users.noreply.github.com>
2021-01-30 21:31:45 +0300

    header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<Header />
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	import Header from "@/components/Header";
+	export default {
+		name: 'App',
+		components: {Header}
+	}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap');
+	* {
+		margin: 0;
+
+		--color-light: #EFEFEF;
+		--color-accent: #66C05D;
+		--color-dark: #333333;
+	}
+	body {
+		background-color: var(--color-light);
+
+		font-family: 'Roboto', sans-serif;
+		font-size: 14px;
+		color: var(--color-dark)
+	}
+	a {
+		color: inherit;
+		text-decoration: none;
+	}
+	hr {
+		border: 1px solid var(--color-light)
+	}
+	.wrapper {
+		max-width: 1110px;
+		margin: 0 auto;
+		padding: 0 1rem;
+	}
+	.button {
+		padding: .7rem 1.8rem;
+		background-color: transparent;
+		border: 2px solid var(--color-accent);
+		border-radius: 1000px;
+	}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,111 @@
+<template>
+	<header>
+		<nav class="nav">
+			<div class="wrapper">
+				<ul>
+					<li><a href="#">Доставка</a></li>
+					<li><a href="#">Оплата</a></li>
+					<li><a href="#">Отзывы</a></li>
+					<li><a href="#">Вопрос-ответ</a></li>
+					<li><a href="#">Контакты</a></li>
+				</ul>
+			</div>
+		</nav>
+
+		<div class="bg-white">
+			<div class="wrapper">
+				<div class="header">
+					<div class="logo">
+						<img :src="cat" alt="logo">
+						BEAUTY
+					</div>
+
+					<div class="back-call">
+						<span class="phone">8-999-888-77-66</span>
+						<button class="button">Обратный звонок</button>
+					</div>
+				</div>
+			</div>
+			<hr>
+			<nav class="main-menu">
+				<div class="wrapper">
+					<ul>
+						<li><a href="#">Парикмахерская</a></li>
+						<li><a href="#">Барбершоп</a></li>
+						<li><a href="#">Маникюр</a></li>
+						<li><a href="#">Педикюр</a></li>
+						<li><a href="#">Массаж</a></li>
+						<li><a href="#">Мебель</a></li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+	</header>
+</template>
+
+<script>
+	import logo from '../assets/img/logo.svg'
+	import cat from '../assets/img/cat.svg'
+	export default {
+		name: "Header",
+		data() {
+			return {
+				logo,
+				cat
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	header {
+
+	}
+	.nav {
+		padding: 1.1rem 0px;
+	}
+	ul {
+		display: flex;
+		justify-content: center;
+
+		list-style-type: none;
+		padding: 0;
+	}
+	li {
+		margin: 0 1rem;
+	}
+	hr {
+		margin: 1.1rem -0px;
+	}
+	.bg-white {
+		background-color: #fff;
+		padding: 1.4rem 0;
+	}
+	.header {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+	.logo {
+		display: flex;
+		align-items: center;
+
+		color: var(--color-accent);
+		font-weight: bold;
+		font-size: 2.6rem;
+		letter-spacing: 0.1em;
+	}
+	.logo > img {
+		width: 80px;
+	}
+
+	.phone {
+		font-size: 1.14rem;
+		margin-right: 2.9rem;
+	}
+
+	.main-menu {
+		font-size: 1rem;
+		color: black;
+	}
+</style>
